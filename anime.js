75ef1780ac63e054076dd758038b1ec2dfdcdabb
StasIpatov <166314021+StasIpatov@users.noreply.github.com>
2024-11-10 22:06:45 +0200

    Add files via upload

--- anime.js
+++ anime.js
@@ -1,18 +1,19 @@
 
-  
     let logo1 = anime({
       targets: '.gif',
       keyframes: [
-        {
-          scale: 1.2,
+        50%{
+          scale: 0.5,
+          rotate: '2turn',
         },
-        {
+        100%{
           scale: 1,
+          rotate: '2turn',
         }
       ],
       duration: 2000,
-      // easing: "linear",
-      autoplay: false, 
+      easing: "linear",
+      autoplay: true, 
     })
     
     document.querySelector('.gif') .addEventListener('click', logo1.play)
